cfd44242c7eb84b025ce0c4dfe87dcd58709adc7
khoadoanduy <khoadoanduy05@gmail.com>
2024-06-29 18:17:13 -0500

    try to create a schema

--- app/(models)/Watchlist.js
+++
@@ -1,9 +0,0 @@
-const mongoose = require('mongoose');
-
-const watchlistSchema = new mongoose.Schema({
-  userId: { type: mongoose.Schema.Types.ObjectId, ref: 'User', required: true },
-  ticker: { type: String, required: true },
-  lastNotifiedPrice: { type: Number }
-});
-
-module.exports = mongoose.models.Watchlist || mongoose.model('Watchlist', watchlistSchema);
